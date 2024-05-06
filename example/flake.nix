156780276e2d0e082dd9f460d783b6d2c534ef51
shivaraj-bh <sbh69840@gmail.com>
2024-05-06 13:10:53 +0530

    reproduce postHook bug

--- example/flake.nix
+++ example/flake.nix
@@ -23,6 +23,9 @@
             dataFile = "data.sqlite";
           in
           {
+            postHook = ''
+              echo "I am postHook"
+            '';
             # httpServer.enable = true;
             settings = {
               environment = {
@@ -31,47 +34,12 @@
 
               processes = {
                 # Print a pony every 2 seconds, because why not.
-                ponysay.command = ''
-                  while true; do
-                    ${lib.getExe pkgs.ponysay} "Enjoy our sqlite-web demo!"
-                    sleep 2
-                  done
-                '';
-
-                # Create .sqlite database from chinook database.
-                sqlite-init.command = ''
-                  echo "$(date): Importing Chinook database (${dataFile}) ..."
-                  ${lib.getExe pkgs.sqlite} "${dataFile}" < ${inputs.chinookDb}/ChinookDatabase/DataSources/Chinook_Sqlite.sql
-                  echo "$(date): Done."
-                '';
-
-                # Run sqlite-web on the local chinook database.
-                sqlite-web = {
+                ponysay = {
                   command = ''
-                    ${pkgs.sqlite-web}/bin/sqlite_web \
-                      --password \
-                      --port ${builtins.toString port} "${dataFile}"
+                    ${lib.getExe pkgs.ponysay} "Enjoy our sqlite-web demo!"
+                    exit 1
                   '';
-                  # The 'depends_on' will have this process wait until the above one is completed.
-                  depends_on."sqlite-init".condition = "process_completed_successfully";
-                  readiness_probe.http_get = {
-                    host = "localhost";
-                    inherit port;
-                  };
-                };
-
-                # If a process is named 'test', it will be ignored. But a new
-                # flake check will be created that runs it so as to test the
-                # other processes.
-                test = {
-                  command = pkgs.writeShellApplication {
-                    name = "sqlite-web-test";
-                    runtimeInputs = [ pkgs.curl ];
-                    text = ''
-                      curl -v http://localhost:${builtins.toString port}/
-                    '';
-                  };
-                  depends_on."sqlite-web".condition = "process_healthy";
+                  availability.exit_on_end = true;
                 };
               };
             };
